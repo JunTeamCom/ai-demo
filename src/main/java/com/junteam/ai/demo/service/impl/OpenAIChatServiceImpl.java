b4271093b4978f6d6407290d54035aa958d27130
gujun <gujun@live.hk>
2026-03-25 17:36:42 +0800

    Add Default Advisor

--- src/main/java/com/junteam/ai/demo/service/impl/OpenAIChatServiceImpl.java
+++ src/main/java/com/junteam/ai/demo/service/impl/OpenAIChatServiceImpl.java
@@ -7,29 +7,33 @@ import org.springframework.stereotype.Service;
 
 import com.junteam.ai.demo.model.ChatAnswer;
 import com.junteam.ai.demo.model.ChatQuestion;
+import com.junteam.ai.demo.service.ChatRulesService;
 import com.junteam.ai.demo.service.ChatService;
 
 @Service
 public class OpenAIChatServiceImpl implements ChatService {
 
     private final ChatClient chatClient;
+    private final ChatRulesService chatRulesService;
 
-    public OpenAIChatServiceImpl(ChatClient.Builder chatClientBuilder) {
+    public OpenAIChatServiceImpl(ChatClient.Builder chatClientBuilder, ChatRulesService chatRulesService) {
         this.chatClient = chatClientBuilder.build();
+        this.chatRulesService = chatRulesService;
     }
 
     @Value("classpath:/promptTemplates/questionPromptTemplate.st")
     Resource questionPromptTemplate;
 
-    @SuppressWarnings("null")
     @Override
     public ChatAnswer ask(ChatQuestion chatQuestion) {
-        var answer = chatClient.prompt()
-                .user(userSpec -> userSpec
-                    .text(questionPromptTemplate)
-                    .param("title", chatQuestion.title())
-                    .param("question", chatQuestion.question())
-                )
+        var chatRules = chatRulesService.getRulesFor(chatQuestion.title(), chatQuestion.question());
+        var answer = chatClient
+                .prompt()
+                .system(systemSpec -> systemSpec
+                        .text(questionPromptTemplate)
+                        .param("countryTitle", chatQuestion.title())
+                        .param("rules", chatRules))
+                .user(chatQuestion.question())
                 .call();
         var answerText = answer.content();
         return new ChatAnswer(chatQuestion.title(), answerText);
