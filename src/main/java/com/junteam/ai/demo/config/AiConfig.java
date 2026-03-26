aed9689a299231e9e8a853bf0f535d349e48b87a
gujun <gujun@live.hk>
2026-03-26 17:01:47 +0800

    Add Transformer and Expander

--- src/main/java/com/junteam/ai/demo/config/AiConfig.java
+++ src/main/java/com/junteam/ai/demo/config/AiConfig.java
@@ -2,6 +2,9 @@ package com.junteam.ai.demo.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,23 @@ public class AiConfig {
         var advisor = RetrievalAugmentationAdvisor.builder()
                 .documentRetriever(VectorStoreDocumentRetriever.builder()
                         .vectorStore(vectorStore)
+                        .build())
+                // 对聊天的问题进行加工处理
+                .queryTransformers(
+                        // 翻译问题
+                        TranslationQueryTransformer.builder()
+                                .chatClientBuilder(chatClientBuilder)
+                                .targetLanguage("Chinese")
+                                .build(),
+                        // 重写（简化）问题
+                        RewriteQueryTransformer.builder()
+                                .chatClientBuilder(chatClientBuilder)
+                                .build())
+                // 扩展相关话题（扩展5个）
+                .queryExpander(MultiQueryExpander.builder()
+                        .chatClientBuilder(chatClientBuilder)
+                        .numberOfQueries(5)
+                        .includeOriginal(false)
                         .build())
                 .build();
         // 设置顾问配置项
