5d8200889fbaf9d2adda098eef824da285fb3986
gujun <gujun@live.hk>
2026-04-01 21:59:59 +0800

    Add Vector Store Chat Memory

--- src/main/java/com/junteam/ai/demo/config/AiConfig.java
+++ src/main/java/com/junteam/ai/demo/config/AiConfig.java
@@ -1,15 +1,8 @@
 package com.junteam.ai.demo.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
-import org.springframework.ai.chat.memory.repository.jdbc.MysqlChatMemoryRepositoryDialect;
+import org.springframework.ai.chat.client.advisor.vectorstore.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -18,32 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AiConfig {
     @Bean
-    ChatMemoryRepository chatMemoryRepository(DataSource dataSource) {
-        return JdbcChatMemoryRepository.builder()
-            .dialect(new MysqlChatMemoryRepositoryDialect())
-            .dataSource(dataSource)
-            .build();
-    }
-
-    @Bean
-    ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
-        return MessageWindowChatMemory.builder()
-            // 最大条数50，即：提问25条、回答25条
-            .chatMemoryRepository(chatMemoryRepository)
-            .maxMessages(50)
-            .build();
-    }
-
-    @Bean
     ChatClient chatClient(ChatClient.Builder chatClientBuilder,
-        ChatMemory chatMemory,
         VectorStore vectorStore) {
         // 设置顾问配置项
         return chatClientBuilder
             .defaultAdvisors(
-                MessageChatMemoryAdvisor.builder(chatMemory)
+                VectorStoreChatMemoryAdvisor
+                    .builder(vectorStore)
                     .build(),
-                QuestionAnswerAdvisor.builder(vectorStore)
+                QuestionAnswerAdvisor
+                    .builder(vectorStore)
                     .searchRequest(SearchRequest.builder().build())
                     .build())
             .build();
